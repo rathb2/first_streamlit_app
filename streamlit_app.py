e960835149da51db456aae74e952868a47122f73
rathb2 <133208703+rathb2@users.noreply.github.com>
2023-05-11 16:22:46 +0530

    Update streamlit_app.py
    added pandas library and some new code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
+import pandas
 
 streamlit.title('this is first streamlit')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 streamlit.header('this is the menu')
 streamlit.text('add different texts')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
