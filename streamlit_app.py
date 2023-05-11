b5f3d4008bbe70ad2c80e1dabe2145654352c986
rathb2 <133208703+rathb2@users.noreply.github.com>
2023-05-11 16:19:52 +0530

    Update streamlit_app.py
    added another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
 streamlit.title('this is first streamlit')
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('this is the menu')
 streamlit.text('add different texts')
