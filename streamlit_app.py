8c594a559e574caf26b47a6777cb27d478d949d9
rathb2 <133208703+rathb2@users.noreply.github.com>
2023-05-18 18:21:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ try:
     # write your own comment - what does this do?
     streamlit.dataframe(fruityvice_normalized)
   
-  except URLError as e:
+except URLError as e:
     streamlit.error()
 
 streamlit.stop()
